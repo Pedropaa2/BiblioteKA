a1c9c8956dff99a6c7cb47a6f3eeaff3bb368cbd
Pedropaa <pedropaa2@gmail.com>
2023-07-03 16:54:14 -0300

    Relacionamentos corrigidos

--- book/views.py
+++ book/views.py
@@ -1,11 +1,21 @@
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from users.models import User
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
 class BookListCreateView(generics.ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [AllowAny]
     queryset = Book.objects.all()
+
     serializer_class = BookSerializer
+
+    def perform_create(self, serializer):
+        print(serializer)
+        serializer.save(bookOwner=self.request.user)
 
 
 class BookRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):

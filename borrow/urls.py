123b9ec24b0d58b23807183b576813dd4f30af42
felipe <felipe@hcdesenvolvimento.com.br>
2023-07-03 14:11:00 -0300

    fix: url borrow

--- borrow/urls.py
+++ borrow/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import BorrowingBookView, BorrowingBookDetailView
 
 urlpatterns = [
-    path("borrowing-book/", BorrowingBookView.as_view()),
-    path("borrowing-book/<int:pk>/", BorrowingBookDetailView.as_view()),
+    path("borrowing_book/", BorrowingBookView.as_view()),
+    path("borrowing_book/<int:pk>/", BorrowingBookDetailView.as_view()),
 ]

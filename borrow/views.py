136529a05e24b55807722262ba91ae7622126c62
Djeff <djeff.walla99@gmail.com>
2023-07-04 12:00:39 -0300

    fix: View Permissions

--- borrow/views.py
+++ borrow/views.py
@@ -1,16 +1,21 @@
 from rest_framework import generics
+
+from app_copy.permissions import AssociateOnlyPermission
 from .models import Borrow
 from .serializers import BorrowSerializer
-
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class BorrowingBookView(generics.ListCreateAPIView):
     queryset = Borrow.objects.filter(is_returned=False)
     serializer_class = BorrowSerializer
-
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [AssociateOnlyPermission]
 
 class BorrowingBookDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Borrow.objects.all()
     serializer_class = BorrowSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [AssociateOnlyPermission]
 
     def perform_destroy(self, instance):
         if instance.is_retuned:

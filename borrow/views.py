cdff63fe48531ffbbc95347ea4f234886c4b766e
Flavia <flaviamaciu@gmail.com>
2023-07-06 17:17:50 -0300

    feat/add schema and update serializer

--- borrow/views.py
+++ borrow/views.py
@@ -3,16 +3,19 @@ from .models import Borrow
 from .serializers import BorrowSerializer, BorrowDetailsSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.permissions import IsAuthenticated
+from app_copy.permissions import AssociateOnlyPermission
 from users.models import User
 from app_copy.models import Copy
-from app_copy.serializers import CopySerializer
+from drf_spectacular.utils import extend_schema
 
 
 class BorrowBooksView(generics.ListAPIView):
     queryset = Borrow.objects.all()
     serializer_class = BorrowSerializer
 
+    @extend_schema(operation_id='borrow_get',
+                   description='Mostrar todos os emprestimos de livros',
+                   tags=['Borrow'])
     def get_queryset(self):
         is_returned = self.request.query_params.get("is_returned")
 
@@ -31,11 +34,14 @@ class BorrowBooksView(generics.ListAPIView):
 
 class BorrowingBookView(generics.CreateAPIView):
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AssociateOnlyPermission]
 
     queryset = Borrow.objects.filter(is_returned=False)
     serializer_class = BorrowSerializer
 
+    @extend_schema(operation_id='borrow_post',
+                   description='Criar emprestimo de livro',
+                   tags=['Borrow'])
     def perform_create(self, serializer):
 
         user = get_object_or_404(User, username=self.kwargs.get("username"))
@@ -52,12 +58,15 @@ class BorrowingBookView(generics.CreateAPIView):
 
 class BorrowingBookDetailView(generics.RetrieveUpdateDestroyAPIView):
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AssociateOnlyPermission]
 
     queryset = Borrow.objects.all()
     serializer_class = BorrowDetailsSerializer
     lookup_url_kwarg = "pk"
 
+    @extend_schema(operation_id='borrow_delete',
+                   description='Soft delete do emprestimo de livro',
+                   tags=['Borrow'])
     def perform_destroy(self, instance):
         copy_table = Copy.objects.get(id=instance.copy_id)
 

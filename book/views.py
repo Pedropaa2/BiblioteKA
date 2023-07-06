1f1cad970602f83541d1bb13ea4fda24e444e054
Djeff <djeff.walla99@gmail.com>
2023-07-06 17:53:09 -0300

    feat: emailFollow

--- book/views.py
+++ book/views.py
@@ -6,7 +6,7 @@ from .serializers import BookSerializer, UserBooksSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .models import Book, UserBooks
-
+from django.core.mail import send_mail
 
 class BookListCreateView(generics.ListCreateAPIView):
     authentication_classes = [JWTAuthentication]
@@ -18,7 +18,7 @@ class BookListCreateView(generics.ListCreateAPIView):
 
 class FollowBookView(generics.ListCreateAPIView):
     authentication_classes = [JWTAuthentication]
-    permission_classes = [AllowAny]
+    permission_classes = [AssociateOnlyPermission]
     queryset = UserBooks.objects.all()
     serializer_class = UserBooksSerializer
 
@@ -30,10 +30,17 @@ class FollowBookView(generics.ListCreateAPIView):
     def create(self, request, *args, **kwargs):
         response = super().create(request, *args, **kwargs)
         book_id = self.kwargs["pk"]
-        users_following = UserBooks.objects.filter(book_id=book_id).values_list(
-            "follow__username", flat=True
-        )
-        response.data["followers"] = list(users_following)
+        users_following = UserBooks.objects.filter(book_id=book_id).values_list("follow__username", flat=True)
+        response.data["users_following"] = list(users_following)
+        
+        # Envio de e-mail para o usuário
+        book = Book.objects.get(id=book_id)
+        subject = "Novo seguidor de livro"
+        message = f"Você tem um novo seguidor para o livro {book.name}!"
+        from_email = "djeff.walla99@gmail.com"
+        to_email = self.request.user.email
+        send_mail(subject, message, from_email, [to_email])
+        
         return response
 
 
